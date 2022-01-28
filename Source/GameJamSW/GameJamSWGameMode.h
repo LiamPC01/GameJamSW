481658c33ad3d5456c0d2b6bfb4ead412f9451ff
Liam Mitchell <liam14mitchell@gmail.com>
2022-01-28 21:26:10 +0000

    Initial commit

---
+++ Source/GameJamSW/GameJamSWGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameJamSWGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGameJamSWGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameJamSWGameMode();
+};
+
+
+
