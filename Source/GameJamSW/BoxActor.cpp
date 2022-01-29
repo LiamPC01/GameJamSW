48aacb483f08b442484084e62af9b09f2c19aa46
Liam Mitchell <liam14mitchell@gmail.com>
2022-01-29 00:52:18 +0000

    Created BoxActor with collision detection
    LM

---
+++ Source/GameJamSW/BoxActor.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BoxActor.h"
+#include "Components/BoxComponent.h"
+#include "Engine/Engine.h"
+
+// Sets default values
+ABoxActor::ABoxActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	CollisionBox->SetBoxExtent(FVector(32.f, 32.f, 32.f));
+	CollisionBox->SetCollisionProfileName("Trigger");
+	RootComponent = CollisionBox;
+
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ABoxActor::OnOverlapBegin);
+	CollisionBox->OnComponentEndOverlap.AddDynamic(this, &ABoxActor::OnOverlapEnd);
+}
+
+// Called when the game starts or when spawned
+void ABoxActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ABoxActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABoxActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, "Overlap Begin Function Called");
+}
+
+void ABoxActor::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, "Overlap End Function Called");
+}
+
